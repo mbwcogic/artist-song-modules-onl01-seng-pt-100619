63489911c45418f2e53a526e0c866e694ee707a5
Michael Waldo <mbwcogic@yahoo.com>
2019-11-19 20:11:36 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,6 +1,7 @@
-module Findable 
-  
-   def self.find_by_name(name)
-    self.all.detect{|o| o.name == name}
+module Findable
+  module ClassMethods
+    def find_by_name(name)
+      self.all.detect{|o| o.name}
+    end    
   end
 end
